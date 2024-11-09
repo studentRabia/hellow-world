cd4e6b6698bf439f85dbd8d9bfa496e4e660b3eb
studentRabia <pubgali125.com@gmail.com>
2024-11-09 21:58:28 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <>
+ 
+ <div className="flex items-center justify-center h-screen bg-green-100">
+      <h1 className="bg-yellow-50 text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-semibold font-serif text-gray-950 shadow-lg md:shadow-2xl border border-gray-300 p-4 sm:p-6 md:p-8 lg:p-10 rounded-lg">
+        Hello World
+      </h1>
+    </div>
+
+   </>
+  );
+}
